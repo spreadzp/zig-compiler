b948d4f6413b17cda498021b244074ff7b206fc3
Paul Spread <spreadzp@gmail.com>
2025-01-02 18:17:11 +0200

    rework UML

--- src/main.zig
+++ src/main.zig
@@ -2,9 +2,11 @@ const std = @import("std");
 const BPFInterpreter = @import("BPFInterpreter.zig").BPFInterpreter;
 
 pub fn main() void {
-    var interpreter = BPFInterpreter.init();
-    const code = [_]u8{ 0x00, 0x02 }; // Пример BPF-кода (ADD и AND)
-    interpreter.execute(&code);
+    var memory = [_]u8{0} ** 12; // 12-byte memory
+    var stack = [_]u8{0} ** 12; // 12-byte stack
+    var interpreter = BPFInterpreter.init(&memory, &stack);
+    const code = [_]u8{ 0x00, 0x02, 0x12 }; // Пример BPF-кода (ADD , AND, EXIT)
+    const valid = interpreter.verifyProgram(&code);
 
-    std.debug.print("Result: {}\n", .{interpreter.registers.r0});
+    std.debug.print("Result: {}\n", .{valid});
 }
